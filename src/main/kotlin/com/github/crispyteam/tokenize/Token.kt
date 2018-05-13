75e8b7fcb5aeb759675390223530296244ba1362
Jan Völker <jan-voelker@t-online.de>
2018-05-14 01:17:21 +0200

    TokenTypes

--- src/main/kotlin/com/github/crispyteam/tokenize/Token.kt
+++ src/main/kotlin/com/github/crispyteam/tokenize/Token.kt
@@ -1,10 +1,25 @@
 package com.github.crispyteam.tokenize
 
 enum class TokenType {
-    // Single character
+    /* Single character */
     PLUS,
     MINUS, STAR, SLASH, PERCENT, BANG,
     EQUALS, SMALLER, GREATER, OPEN_BRACKET, CLOSE_BRACKET, DOT,
     SEMICOLON, OPEN_PAREN, CLOSE_PAREN, OPEN_BRACE,
-    CLOSE_BRACE, COMMA, COLON
+    CLOSE_BRACE, COMMA, COLON,
+
+    /* Two character */
+    EQUALS_EQUALS,
+    BANG_EQUALS,
+    SMALLER_EQUALS, GREATER_EQUALS,
+
+    /* Literals */
+    STRINGS,
+    NUMBER, IDENTIFIER,
+
+    /* Keywords */
+    FUN,
+    VAL, VAR, CONTINUE, BREAK, IF, ELSE, FOR, WHILE, IMPORT, IN, RETURN, TRUE, FALSE, NIL, AND, OR,
+
+    EOF
 }
