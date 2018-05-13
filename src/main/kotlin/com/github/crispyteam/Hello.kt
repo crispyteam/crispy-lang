c9ac5a9b7a4f09cb0c60408446fa9c1404bac33b
Felix <felixschoeller@outlook.de>
2018-05-14 00:57:35 +0200

    init src

---
+++ src/main/kotlin/com/github/crispyteam/Hello.kt
@@ -0,0 +1,6 @@
+package com.github.crispyteam
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ src/test/kotlin/com/github/crispyteam/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.github.crispyteam
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
