6284548c2b9607425938bf4d61e92f98a6eff40e
Felix <felixschoeller@outlook.de>
2018-05-14 01:07:04 +0200

    single char token types

---
+++ src/main/kotlin/com/github/crispyteam/tokenize/Token.kt
@@ -0,0 +1,10 @@
+package com.github.crispyteam.tokenize
+
+enum class TokenType {
+    // Single character
+    PLUS,
+    MINUS, STAR, SLASH, PERCENT, BANG,
+    EQUALS, SMALLER, GREATER, OPEN_BRACKET, CLOSE_BRACKET, DOT,
+    SEMICOLON, OPEN_PAREN, CLOSE_PAREN, OPEN_BRACE,
+    CLOSE_BRACE, COMMA, COLON
+}
