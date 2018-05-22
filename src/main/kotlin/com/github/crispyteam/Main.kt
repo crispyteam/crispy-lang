f370de463e2f8855ecd9a2d5bf28dd946ade75a8
funkschy <felixschoeller@outlook.de>
2018-05-22 13:03:01 +0200

    Revert "Multiline repl"
    This reverts commit c7297cd

--- src/main/kotlin/com/github/crispyteam/Main.kt
+++ src/main/kotlin/com/github/crispyteam/Main.kt
@@ -8,16 +8,12 @@ import java.util.*
 import kotlin.system.exitProcess
 
 const val version = "0.1"
-private var hadError = false
+private const val prompt = ">>> "
 
+private var hadError = false
 private val interpreter = Interpreter()
 internal var inRepl = false
 internal var replLine = 1
-private var prompt = ">>> "
-
-private var openParen = 0
-private var openBrace = 0
-private var openBracket = 0
 
 fun main(args: Array<String>) =
         when {
@@ -57,21 +53,12 @@ fun runInteractive() {
     println("Crispy interactive shell version $version")
 
     val scanner = Scanner(System.`in`)
-    var code = ""
 
     while (true) {
         print(prompt)
         try {
             val line = scanner.nextLine()
-            code += line
-            analyse(line)
-            prompt = if (openBrace == 0 && openBracket == 0 && openParen == 0) {
-                run(code)
-                code = ""
-                ">>> "
-            } else {
-                "... "
-            }
+            run(line)
             replLine++
         } catch (e: RuntimeException) {
             // scanner throws an annoying exception when repl is quit with EOF char
@@ -81,15 +68,6 @@ fun runInteractive() {
 
         hadError = false
     }
-}
-
-/**
- * Updates the number of open Parentheses, Brackets and Braces.
- */
-private fun analyse(line: String) {
-    openParen += line.filter { it == '(' }.count() - line.filter { it == ')' }.count()
-    openBrace += line.filter { it == '{' }.count() - line.filter { it == '}' }.count()
-    openBracket += line.filter { it == '[' }.count() - line.filter { it == ']' }.count()
 }
 
 fun run(program: String) {
